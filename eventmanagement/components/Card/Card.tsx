aeab5a2e000963c01fd55a2028a7ce0c137f14dd
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2025-08-25 00:03:42 +0530

    edited card

--- eventmanagement/components/Card/Card.tsx
+++ eventmanagement/components/Card/Card.tsx
@@ -15,7 +15,8 @@ const Card = ({
       <div className="h-[350px] flex flex-col justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
         <div>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {name}
+            {name.slice(0, 100)}
+            {name.length >= 100 && "...."}
           </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 ">
             {description.slice(0, 300)}
