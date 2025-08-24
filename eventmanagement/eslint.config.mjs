dc438e50a642ad91d49e824244350d1fd8dc2e8d
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2025-08-24 23:50:49 +0530

    new update config

--- eventmanagement/eslint.config.mjs
+++ eventmanagement/eslint.config.mjs
@@ -20,7 +20,9 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      // "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
